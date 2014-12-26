2c186374f6a39e02de3e8837a594c9e7bc758f1e
thirdcreed <ccreed@CalebCreed-HP.telogical.com>
2014-12-26 09:41:45 -0600

    adds unwritten candidates to the dominance matrix

--- index.js
+++ index.js
@@ -16,16 +16,30 @@ var Profile = function () {
       }
 
      var updateDominanceMatrix = _.bind(function updateDominanceMatrix(ordering){ 
-	var num_ordering = _.map(ordering, function(item){
+	
+	var unwrittenCandidates = _.difference(this.candidates,ordering);
+
+	var numOrdering = _.map(ordering, function(item){
 	    return candidateMap[item];
 	});
         
 	for(var i=0;i < ordering.length;i++){
 	    for(var j=i; j < ordering.length;j++){
-	       if(i != j) {this.dominanceMatrix[num_ordering[i]][num_ordering[j]]++;}
+	       if(i != j){
+	           this.dominanceMatrix[numOrdering[i]][numOrdering[j]]++;
+	       }
 	    }
-	} 
+	}
+
+	for(var ii=0; ii < unwrittenCandidates.length;ii++){
+	    var unwritten = candidateMap[unwrittenCandidates[ii]]; 
+	    for(var jj=0; jj < numOrdering.length;jj++){
+	        this.dominanceMatrix[numOrdering[jj]][unwritten]++; 		    
+	   }	
+	}
+	
      },this);
+
     var candidateMap ={}; 
     this.candidates = [];
     this.data = [];
@@ -48,8 +62,10 @@ var Profile = function () {
          
 	var key = ordering.join("_");
 	var priorVote = _.where(this.data,{"key":key})[0];
+	
 	updateDominanceMatrix(ordering);
-        if (priorVote) {
+   
+   	if (priorVote) {
 	    priorVote.numVotes++;
 	    return;
 	}
