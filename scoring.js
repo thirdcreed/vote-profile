4d025eb55d89bee6366b056a30f74f19bc1db55a
thirdcreed <ccreed@CalebCreed-HP.telogical.com>
2014-12-28 08:55:22 -0600

    adds tie logic to all methods, refactors condercet method

--- scoring.js
+++ scoring.js
@@ -1,5 +1,7 @@
 var copy = require('shallow-copy');
 var _ = require('lodash');
+var condercet = require('./condercet.js');
+
 module.exports = function score(scoreMethod) {
 
     scoreMethod = scoreMethod || "borda";
@@ -21,7 +23,14 @@ var common = {
 
     borda: function borda(P) {
         var score = getBlankScores(P.candidates);
+
         P.each(function (obj, val, i) {
+            if (_.isArray(val)) {
+                _.each(val, function (tied) {
+                    score[tied] += ((P.candidates.length - i) * obj.numVotes);
+                });
+                return;
+            }
             score[val] += ((P.candidates.length - i) * obj.numVotes);
         });
 
@@ -29,20 +38,45 @@ var common = {
     },
 
     plurality: function plurality(P) {
+
         var score = getBlankScores(P.candidates);
+
         P.each(function (obj, val, i) {
+            if (_.isArray(val)) {
+                _.each(val, function (tied) {
+                    score[tied] += (i === 0) ? obj.numVotes : 0;
+                });
+                return;
+            }
             score[val] += (i === 0) ? obj.numVotes : 0;
         });
 
         return score;
     },
+
     veto: function veto(P) {
+
         var score = getBlankScores(P.candidates);
+
         P.each(function (obj, val, i) {
+            if (_.isArray(val)) {
+                _.each(val, function (tied) {
+                    score[tied] += obj.numVotes;
+                });
+                return;
+            }
             score[val] += obj.numVotes;
         });
 
         return score;
-    }
+    },
 
+    black: function black(P) {
+
+        var condercetWinners = condercet(P);
+        var bordaWinners = this.borda(P);
+
+        return _.isEmpty(condercetWinners) ? bordaWinners : condercetWinners;
+
+    }
 };
--- tests/test.js
+++ tests/test.js
@@ -2,37 +2,90 @@ var assert = require('assert');
 var Profile = require("../index.js");
 
 
-describe('Profile',function(){
-  var p = new Profile();
-   p.setCandidates(["a","b","c","d"]); 
-   p.vote(["a","b","c","d"]);
-   p.vote(["b","c","a","d"]);
-   p.vote(["b","c","d","a"]);
-   p.vote(["c","d","b","a"]);
-  
- describe("#data",function(){
-   it("should have the correct vote aggregation data",function(){
-       assert.equal(1,p.data[0].numVotes);
-   });
- });
+describe('Profile', function () {
 
- describe("#score",function(){
-  
-  it('should correctly score with the borda method',function(){
-     assert.deepEqual({"a":8,"b":13,"c":12,"d":7},p.score("borda"));
-     });
- 
-  it('should correctly score with the plurlality method',function(){
-     assert.deepEqual({"a":1,"b":2,"c":1,"d":0},p.score("plurality"));
-     });
+    var noExtensionsWithTies = new Profile();
+    var condercetNoTies = new Profile();
+    var condercetWithTies = new Profile();
+    var approvalNoTies = new Profile();
 
-   it('should correctly score with the veto method',function(){
-     assert.deepEqual({"a":4,"b":4,"c":4,"d":4},p.score("veto"));
-     });
+    condercetNoTies.extend("condercet");
+    condercetWithTies.extend("condercet");
+    approvalNoTies.extend("approval");
 
-   it('should correctly score with the black method',function(){
-     assert.deepEqual({"a":0,"b":1,"c":0,"d":0},p.score("black"));
-     });
+    noExtensionsWithTies.setCandidates(["a", "b", "c", "d"]);
+    noExtensionsWithTies.vote([["a", "b"], "c", "d"]);
+    noExtensionsWithTies.vote(["b", "c", "a", "d"]);
+    noExtensionsWithTies.vote(["b", "c", "d", "a"]);
+    noExtensionsWithTies.vote(["c", "d", "b", "a"]);
 
- });
+    condercetNoTies.setCandidates(["a", "b", "c"]);
+    condercetNoTies.vote(["a", "b", "c"]);
+    condercetNoTies.vote(["a", "b", "c"]);
+    condercetNoTies.vote(["b", "a", "c"]);
+
+    condercetWithTies.setCandidates(["a", "b", "c"]);
+    condercetWithTies.vote(["a", "b", "c"]);
+    condercetWithTies.vote(["a", "b", "c"]);
+    condercetWithTies.vote(["b", "a", "c"]);
+
+
+
+    describe("#data", function () {
+        it("should have the correct vote aggregation data", function () {
+            assert.equal(1, noExtensionsWithTies.data[0].numVotes);
+        });
+    });
+
+    describe("#score", function () {
+
+        it('should correctly score with the borda method', function () {
+            assert.deepEqual({
+                "a": 8,
+                "b": 14,
+                "c": 13,
+                "d": 8
+            }, noExtensionsWithTies.score("borda"));
+        });
+
+        it('should correctly score with the plurlality method', function () {
+            assert.deepEqual({
+                "a": 1,
+                "b": 3,
+                "c": 1,
+                "d": 0
+            }, noExtensionsWithTies.score("plurality"));
+        });
+
+        it('should correctly score with the veto method', function () {
+            assert.deepEqual({
+                "a": 4,
+                "b": 4,
+                "c": 4,
+                "d": 4
+            }, noExtensionsWithTies.score("veto"));
+        });
+
+        it('should correctly score with the black method', function () {
+            assert.deepEqual({
+                "a": 1,
+                "b": 0,
+                "c": 0
+            }, condercetNoTies.score("black"));
+        });
+
+    });
+
+    describe("#extend", function () {
+
+        it('should not have a domniance matrix without first extending ', function () {
+            assert.equal(undefined, noExtensionsWithTies.dominanceMatrix);
+        });
+
+        it('should have a dominance matrix after extending', function () {
+            assert.equal(typeof condercetNoTies.dominanceMatrix, "object");
+        });
+    });
+
+
 });
